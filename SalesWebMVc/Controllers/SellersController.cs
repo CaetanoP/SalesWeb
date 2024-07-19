2690c6d61cd636ea73d80bc8f7b66c40e1c93960
Caetano Passos <caetanof2004@gmail.com>
2024-07-19 16:32:38 -0300

    Validation

--- SalesWebMVc/Controllers/SellersController.cs
+++ SalesWebMVc/Controllers/SellersController.cs
@@ -36,7 +36,13 @@ namespace SalesWebMVc.Controllers
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		public IActionResult Create(Seller seller ) 
-		{ 
+		{
+			if (!ModelState.IsValid)
+			{
+				var departments = _departmentService.FindAll();
+				var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+				return View(viewModel);
+			}
 			//this create is to insert the data
 			_sellerService.Insert(seller);
 			return RedirectToAction(nameof(Index));
@@ -95,6 +101,12 @@ namespace SalesWebMVc.Controllers
 		[ValidateAntiForgeryToken]
 		public IActionResult Edit(int id, Seller seller)
 		{
+			if (!ModelState.IsValid)
+			{
+				var departments = _departmentService.FindAll();
+				var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+				return View(viewModel);
+			}
 			if (id != seller.Id)
 			{
 				return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
