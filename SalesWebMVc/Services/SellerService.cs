242ed8292c327451a9257fed0b2bc90755bc057f
Caetano Passos <caetanof2004@gmail.com>
2024-07-19 14:06:49 -0300

    Delete seller

--- SalesWebMVc/Services/SellerService.cs
+++ SalesWebMVc/Services/SellerService.cs
@@ -3,25 +3,36 @@ using SalesWebMVc.Models;
 
 namespace SalesWebMVc.Services
 {
-    public class SellerService
-    {
-        private readonly SalesWebMVcContext _context;
-        public SellerService(SalesWebMVcContext context)
-        {
-            _context = context;
-        }
+	public class SellerService
+	{
+		private readonly SalesWebMVcContext _context;
+		public SellerService(SalesWebMVcContext context)
+		{
+			_context = context;
+		}
 
-        public List<Seller> FindAll()
-        {
-            
-            return _context.Seller.ToList();
-        }
+		public List<Seller> FindAll()
+		{
 
-        public void Insert(Seller obj)
-        {
+			return _context.Seller.ToList();
+		}
+
+		public void Insert(Seller obj)
+		{
 			_context.Add(obj);
 			_context.SaveChanges();
 		}
 
-    }
+		public Seller FindById(int id)
+		{
+			return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+		}
+		public void Remove(int id)
+		{
+			var obj = _context.Seller.Find(id);
+			_context.Seller.Remove(obj);
+			_context.SaveChanges();
+		}
+
+	}
 }
